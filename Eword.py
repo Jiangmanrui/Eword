b73252bddadaeca80a2432db027635bc3382fae9
Jiang,Manrui <Jiangmanrui@users.noreply.github.com>
2020-02-03 18:30:24 +0800

    Summary
    Eword.py is the main program.
    
    Get words pronunciation from youdao api by youdaoread.py.
    
    '.csv' is the sample input data.

---
+++ Eword.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+import tkinter
+import tkinter.messagebox
+import pandas as pd
+import numpy as np
+import os
+from tkinter import filedialog
+import youdaoread
+from playsound import playsound
+import copy
+
+#加载有道
+yd=youdaoread.youdao()
+
+connect=0
+
+def load():
+    global data
+    global data_address
+    selectFile = tkinter.filedialog.askopenfilename()
+    data = pd.read_csv(selectFile, encoding='gbk')
+    name1=os.path.split(str(selectFile))[1]
+    name2=name1[0:(len(name1)-4)]
+    filename.delete(0.0, 'end')
+    filename.insert('end', name2)
+    data_address=selectFile
+
+def select():
+    #设置一个概率表
+    global glb
+    global connect
+    length=len(data['eword'])
+    glb=0
+    if glb==0:
+        #标准化计数
+        maxc=max(data['count'])
+        minc=min(data['count'])
+        temp=copy.deepcopy(data)
+        temp['count2']=1-(temp['count']-minc)/(maxc-minc+1)
+        #计算单词被选中的得分
+        temp['glb0']=temp['count2']*0.3+temp['proportion']*0.7
+        maxglb=max(temp['glb0'])
+        minglb=min(temp['glb0'])
+        if maxglb==minglb:
+            temp['glb']=1/length
+        else:
+            temp['glb']=temp['glb0']/sum(temp['glb0'])
+        #得到概率表
+        glb=list(temp['glb'])
+    #清空上一次的结果
+    answer.delete(0.0, 'end')
+    answer.insert('end', "...")
+    spell.delete(0.0, 'end')
+    cword.delete(0.0, 'end')
+    #根据概率表进行随机抽取
+    x=np.random.choice(range(length),p=glb)
+    connect=x
+
+#读单词
+def read():
+    word=data.loc[connect,'eword']
+    word2=word.split(" ")
+    for w in word2:
+        yd.down(w)
+    for w in word2:
+        source_file_path="E:/youdaoread/Speech_US/"+w+".mp3"
+        playsound(source_file_path)
+
+#显示英文拼写，并判断对错
+def showe():
+    word=data.loc[connect,'eword']
+    answer.delete(0.0, 'end')
+    answer.insert('end',word)
+    word=word.lower()
+    uspell=spell.get(0.0,tkinter.END)
+    uspell=uspell.lower().rstrip()
+    # print(word,"&&&",uspell,"&&&")
+    data.loc[connect, 'count']=data.loc[connect, 'count']+1
+    if word!=uspell:
+        data.loc[connect, 'wrong']=data.loc[connect, 'wrong']+0.5
+        spell.delete(0.0, 'end')
+        spell.insert('end',uspell,'wrong')
+    else:
+        spell.delete(0.0, 'end')
+        spell.insert('end', uspell, 'zc')
+
+#显示中文意思及历史错误率
+def showc():
+    p = round(data.loc[connect, 'proportion'] * 100, 2)
+    cword.insert('end',str(data.loc[connect, 'cword']) +'\n\n'+ '错误率：' + str(p) + '%')
+
+#中文意思记错，增加错误率
+def cwrong():
+    data.loc[connect, 'wrong']=data.loc[connect, 'wrong']+0.5
+
+
+
+mygui=tkinter.Tk()
+mygui.title('EWORD')
+sw = mygui.winfo_screenwidth()
+sh = mygui.winfo_screenheight()
+
+ww = 1024
+wh = 600
+xx = (sw-ww) / 2
+yy = (sh-wh) / 2
+mygui.geometry('%dx%d+%d+%d'%(ww,wh,xx,yy))
+# text=Text(width=30,height=3)
+
+fm1=tkinter.Frame()
+fm1.pack(side='top',expand='yes',pady=20)
+
+fm11=tkinter.Frame(fm1)
+fm11.pack(side='top',expand='yes',pady=20)
+
+fm12=tkinter.Frame(fm1)
+fm12.pack(side='top',expand='yes',pady=20)
+
+fm2=tkinter.Frame()
+fm2.pack(side='top',expand='yes',pady=20)
+
+fm21=tkinter.Frame(fm2)
+fm21.pack(side='left', fill='x', expand='yes',padx=10)
+
+fm22=tkinter.Frame(fm2)
+fm22.pack(side='right', fill='x', expand='yes')
+
+fm3=tkinter.Frame()
+fm3.pack(side='top',expand='yes',pady=20)
+
+label=tkinter.Label(fm11,text='听力词汇记记记',font=("黑体", 25, "bold")).pack(anchor='center',expand='yes')
+
+loadfile=tkinter.Button(fm12,text='输入文件',width=20,height=2,command=load,font=("黑体", 15, "bold")).pack(side='left',expand='yes',padx=10)
+filename = tkinter.Text(fm12,width=20,height=2,font=("黑体", 15, "bold"))
+filename.pack(expand='yes')
+
+
+#所抽取的单词
+read=tkinter.Button(fm21,text='听',width=19,height=1,command=read,font=("黑体", 20, "bold")).pack(pady=5)
+#答案
+answer = tkinter.Text(fm21,width=20,height=2,font=("黑体", 20, "bold"))
+answer.insert('end',"...")
+answer.pack(anchor='center',side='top',expand='yes')
+#填写
+spell = tkinter.Text(fm21,width=20,height=2,font=("黑体", 20, "bold"))
+spell.pack(anchor='center',side='top',expand='yes')
+
+spell.tag_add('wrong','0.0') #申明一个tag
+spell.tag_config('wrong',foreground='red') #设置tag即插入文字的大小,颜色等
+spell.tag_add('zc','0.0') #申明一个tag
+spell.tag_config('zc',foreground='black') #设置tag即插入文字的大小,颜色等
+
+#中文意思
+cword = tkinter.Text(fm22,width=20,height=6,font=("黑体", 20, "bold"))
+cword.pack(anchor='center',side='top',expand='yes')
+
+bx=10
+bh=2
+next=tkinter.Button(fm3,text='下一个',width=bx,height=bh,command=select,font=("黑体", 20, "bold")).pack(side='left',expand='yes')
+
+seword=tkinter.Button(fm3,text='显示单词',width=bx,height=bh,command=showe,font=("黑体", 20, "bold")).pack(side='left',expand='yes')
+
+scword=tkinter.Button(fm3,text='显示中文',width=bx,height=bh,command=showc,font=("黑体", 20, "bold")).pack(side='left',expand='yes')
+
+cmwrong=tkinter.Button(fm3,text='中文记错',width=bx,height=bh,command=cwrong,font=("黑体", 20, "bold")).pack(side='left',expand='yes')
+
+mygui.mainloop()
+
+# 错误率清算
+length=len(data['eword'])
+for word in range(length):
+    if data.loc[word,'count']==0:
+        data.loc[word, 'proportion']=0
+    else:
+        data.loc[word,'proportion'] = data.loc[word,'wrong'] / data.loc[word,'count']
+data.to_csv(data_address,encoding='gbk',index=False)
---
+++ youdaoread.py
@@ -0,0 +1,113 @@
+'''
+转载自 祥的博客
+原文链接：https://blog.csdn.net/humanking7/article/details/88630856
+
+程序思想：
+有两个本地语音库，美音库Speech_US，英音库Speech_US
+调用有道api，获取语音MP3，存入对应的语音库中
+'''
+
+import os
+import urllib.request
+
+
+class youdao():
+    def __init__(self, type=0, word='hellow'):
+        '''
+        调用youdao API
+        type = 0：美音
+        type = 1：英音
+
+        判断当前目录下是否存在两个语音库的目录
+        如果不存在，创建
+        '''
+        word = word.lower()  # 小写
+        self._type = type  # 发音方式
+        self._word = word  # 单词
+
+        if not os.path.exists('E:\youdaoread'):
+            # 不存在，就创建
+            os.makedirs('E:\youdaoread')
+
+        # 文件根目录
+        self._dirRoot = os.path.dirname(os.path.abspath(__file__))
+        if 0 == self._type:
+            self._dirSpeech = os.path.join(self._dirRoot, 'E:\youdaoread\Speech_US')  # 美音库
+        else:
+            self._dirSpeech = os.path.join(self._dirRoot, 'E:\youdaoread\Speech_EN')  # 英音库
+
+        # 判断是否存在美音库
+        if not os.path.exists('E:\youdaoread\Speech_US'):
+            # 不存在，就创建
+            os.makedirs('E:\youdaoread\Speech_US')
+        # 判断是否存在英音库
+        if not os.path.exists('E:\youdaoread\Speech_EN'):
+            # 不存在，就创建
+            os.makedirs('E:\youdaoread\Speech_EN')
+
+    def setAccent(self, type=0):
+        '''
+        type = 0：美音
+        type = 1：英音
+        '''
+        self._type = type  # 发音方式
+
+        if 0 == self._type:
+            self._dirSpeech = os.path.join(self._dirRoot, 'E:\youdaoread\Speech_US')  # 美音库
+        else:
+            self._dirSpeech = os.path.join(self._dirRoot, 'E:\youdaoread\Speech_EN')  # 英音库
+
+    def getAccent(self):
+        '''
+        type = 0：美音
+        type = 1：英音
+        '''
+        return self._type
+
+    def down(self, word):
+        '''
+        下载单词的MP3
+        判断语音库中是否有对应的MP3
+        如果没有就下载
+        '''
+        word = word.lower()  # 小写
+        tmp = self._getWordMp3FilePath(word)
+        if tmp is None:
+            self._getURL()  # 组合URL
+            # 调用下载程序，下载到目标文件夹
+            # print('不存在 %s.mp3 文件\n将URL:\n' % word, self._url, '\n下载到:\n', self._filePath)
+            # 下载到目标地址
+            urllib.request.urlretrieve(self._url, filename=self._filePath)
+            # print('%s.mp3 下载完成' % self._word)
+        # else:
+            # print('已经存在 %s.mp3, 不需要下载' % self._word)
+
+        # 返回声音文件路径
+        return self._filePath
+
+    def _getURL(self):
+        '''
+        私有函数，生成发音的目标URL
+        http://dict.youdao.com/dictvoice?type=0&audio=
+        '''
+        self._url = r'http://dict.youdao.com/dictvoice?type=' + str(
+            self._type) + r'&audio=' + self._word
+
+    def _getWordMp3FilePath(self, word):
+        '''
+        获取单词的MP3本地文件路径
+        如果有MP3文件，返回路径(绝对路径)
+        如果没有，返回None
+        '''
+        word = word.lower()  # 小写
+        self._word = word
+        self._fileName = self._word + '.mp3'
+        self._filePath = os.path.join(self._dirSpeech, self._fileName)
+
+        # 判断是否存在这个MP3文件
+        if os.path.exists(self._filePath):
+            # 存在这个mp3
+            return self._filePath
+        else:
+            # 不存在这个MP3，返回none
+            return None
